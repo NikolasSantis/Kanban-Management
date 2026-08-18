caca61ffdbaf1003e5a7215fec587d2f5aec2561
NikolasSanits <nicolastigosan@gmail.com>
2026-08-17 21:06:36 -0300

    feat: add-routes-to-workspace-members

--- internal/routes/workspace.go
+++ internal/routes/workspace.go
@@ -14,4 +14,10 @@ func RegisterWorkspaceRoutes(app *fiber.App) {
 	workspace.Post("/", handler.CreateWorkspace())
 	workspace.Patch("/:id", handler.PatchWorkspace())
 	workspace.Delete("/:id", handler.DeleteWorkspace())
+
+	workspace.Get("/member", handler.GetMyWorkspaces())
+	workspace.Get("/member/:id", handler.GetMembersOnWorkspace())
+	workspace.Post("/member/:id", handler.AddMemberToWorkspace())
+	workspace.Patch("/member/:id", handler.PatchWorkspaceMember())
+	workspace.Delete("/member/:id", handler.DeleteWorkspaceMember())
 }
